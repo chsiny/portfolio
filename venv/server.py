3e516a05fbc9cd8cdfc1001d442090d0c3bf0c97
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2023-03-02 16:15:30 +1000

    test

--- venv/server.py
+++ venv/server.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("test.html")
+
+@app.route("/guess/<name>")
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response =requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template("test.html", name=name, gender=gender, age=age)
+
 
 
 if __name__ == "__main__":
